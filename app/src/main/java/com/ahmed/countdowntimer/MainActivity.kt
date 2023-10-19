a51ad9974bf564a44b3ff40eb63750b352a49a65
ahmed elkomy <ahmedelkomy@gmail.com>
2023-10-19 19:28:15 +0300

    Timer

---
+++ app/src/main/java/com/ahmed/countdowntimer/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.ahmed.countdowntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val starttime : Long = 25 * 60 * 1000
+    var reimagining : Long = starttime
+    lateinit var pomo: TextView
+    var timer : CountDownTimer? = null
+    var istimerunning = false
+    lateinit var Time: TextView
+    lateinit var start:Button
+    lateinit var reset:TextView
+    private fun id(){
+        pomo=findViewById(R.id.pomo_tv)
+        Time=findViewById(R.id.time_tv)
+        start=findViewById(R.id.start_btn)
+        reset=findViewById(R.id.reset_tv)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        id()
+
+
+        start.setOnClickListener {
+            if (istimerunning == false) {
+                starttimer()
+                pomo.text = resources.getText(R.string.keep_going)
+            }
+        }
+
+
+        reset.setOnClickListener {
+            resettime()
+        }
+
+
+    }
+
+
+    private fun resettime(){
+        timer?.cancel()
+        reimagining =starttime
+        Updatetime()
+        pomo.text = resources.getText(R.string.Take_a_Pomodor)
+        istimerunning == false
+    }
+
+
+
+    private fun starttimer() {
+         timer = object : CountDownTimer ( 1*starttime , 1 * 1000) {
+
+             override fun onTick(timelift: Long) {
+                reimagining=timelift
+                Updatetime()
+            }
+
+             override fun onFinish() {
+            Toast.makeText(this@MainActivity,"finish",Toast.LENGTH_SHORT).show()
+                 istimerunning = false
+            }
+        }.start()
+        istimerunning = true
+
+    }
+
+
+    private fun Updatetime() {
+        val minute = reimagining.div(1000).div(60)
+        val second = reimagining.div(1000) % 60
+        val formattime = String.format("%02d:%02d", minute, second)
+        Time.text = formattime
+    }
+}
